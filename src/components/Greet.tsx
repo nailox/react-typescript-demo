d800625e7edf018acd00b657f6abc06281a44c05
nailox <nmandov@gmail.com>
2023-01-31 20:33:40 +0200

    initial commit

---
+++ src/components/Greet.tsx
@@ -0,0 +1,7 @@
+export const Greet = () => {
+    return (
+    <div>
+        <h2>hello you have 10 unread messages </h2>
+    </div>
+    )
+}
---
+++ src/test.js
@@ -0,0 +1,29 @@
+function myDisplayer(some) {
+    document.getElementById("demo").innerHTML = some;
+  }
+  
+  let myPromise = new Promise(function(myResolve, myReject) {
+    let x = 0;
+  
+  // The producing code (this may take some time)
+  
+    if (x == 0) {
+      myResolve("OK");
+    } else {
+      myReject("Error");
+    }
+  });
+  
+  myPromise.then(
+    function(value) {myDisplayer(value);},
+    function(error) {myDisplayer(error);}
+  );
+
+  doSomethingCritical()
+  .then((result) =>
+    doSomethingOptional(result)
+      .then((optionalResult) => doSomethingExtraNice(optionalResult))
+      .catch((e) => {}),
+  ) // Ignore if optional stuff fails; proceed.
+  .then(() => moreCriticalStuff())
+  .catch((e) => console.error(`Critical failure: ${e.message}`));
